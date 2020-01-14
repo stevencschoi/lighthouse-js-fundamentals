dbf47995ee9c038a671332c582896294aa6918e7
Steven Choi <stevencschoi87@gmail.com>
2020-01-14 04:14:45 +0000

    adding changes to previous exercises

--- packing-list.js
+++ packing-list.js
@@ -1,8 +1,18 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+// for loops
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
 
 console.log(packingList[3]);
+
+// while loops
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
